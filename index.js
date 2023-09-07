af0bc8ea4a2ec695455fc65cbec56e7c57ec9b52
Jacob Erisman <jacob.erisman@gmail.com>
2023-09-07 12:27:14 -0400

    Lab Completed - JE

--- index.js
+++ index.js
@@ -9,23 +9,20 @@ function moveDodgerLeft() {
   }
 }
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowLeft") {
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e){
+  if (e.key === "ArrowLeft"){
     moveDodgerLeft();
   }
-});
-
-function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left < 361) {
-      dodger.style.left = `${left + 1}px`;
-    }
+  else if (e.key === "ArrowRight"){
+    moveDodgerRight();
   }
-  
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowRight") {
-      moveDodgerRight();
-    }
-  });
+});
